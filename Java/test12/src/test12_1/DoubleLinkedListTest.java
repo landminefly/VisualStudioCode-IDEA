5255fb8d41c36c252eadf97c61b5beea3721f2f5
LandmineFly <2353789859@qq.com>
2023-01-12 12:52:52 +0800

    update list

--- Java/test12/src/test12_1/DoubleLinkedListTest.java
+++ Java/test12/src/test12_1/DoubleLinkedListTest.java
@@ -15,13 +15,16 @@ public class DoubleLinkedListTest {
     }
 }
 
+//双向链表
 class DoubleLinkedList {
     public DoubleNode head;
 
+    //在构造函数中，先创建一个头节点
     public DoubleLinkedList() {
         head = new DoubleNode(0, null, 0);
     }
 
+    //将传入节点加入到链表尾部
     public void add(DoubleNode newNode) {
         DoubleNode temp = head;
         while (temp.next != null) {
@@ -31,6 +34,8 @@ class DoubleLinkedList {
         newNode.previous = temp;
     }
 
+    //按照id的大小排序，将传入节点插入到链表的指定位置
+    //（前提是该链表已存在节点中的id本来就是有序的）
     public void insertById(DoubleNode newNode) {
         DoubleNode temp = head;
         while (true) {
@@ -47,6 +52,8 @@ class DoubleLinkedList {
             temp = temp.next;
         }
         newNode.next = temp.next;
+        //注意：当传入节点插入到链表末尾时，temp.next为空
+        //而在单向链表中，因为不存在previous指针，所以无需考虑这个问题
         if (temp.next != null) {
             temp.next.previous = newNode;
         }
@@ -54,6 +61,7 @@ class DoubleLinkedList {
         newNode.previous = temp;
     }
 
+    //根据传入节点的id，更改链表中相同id节点的数据
     public void update(DoubleNode updatedNode) {
         DoubleNode temp = head;
         while (true) {
@@ -71,6 +79,9 @@ class DoubleLinkedList {
         temp.score = updatedNode.score;
     }
 
+    //删除指定id节点
+    //因为这是双向链表，因此在删除节点时，可以直接定位到要删除节点本身
+    //然后找到它的上一个节点和下一个节点，并完成删除操作
     public void delete(int id) {
         DoubleNode temp = head;
         while (true) {
@@ -85,12 +96,15 @@ class DoubleLinkedList {
             temp = temp.next;
         }
         temp.previous.next = temp.next;
+        //注意：当要删除节点位于链表末尾时，temp.next为空
+        //而在单向链表中，因为不存在previous指针，所以无需考虑这个问题
         if(temp.next != null)
         {
             temp.next.previous = temp.previous;
         }
     }
 
+    //打印链表的所有节点
     public void showAllNodes() {
         DoubleNode temp = head.next;
         if (temp == null) {
@@ -104,10 +118,13 @@ class DoubleLinkedList {
     }
 }
 
+//双向链表节点
 class DoubleNode {
+    //节点存放的数据
     public int id;
     public String name;
     public int score;
+    //节点存放的指针，默认为null，在插入到链表中时才赋值
     public DoubleNode next;
     public DoubleNode previous;
 
