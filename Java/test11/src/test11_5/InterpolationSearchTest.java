0af2c1169ee34df19a1a48502c16545cd9e03302
LandmineFly <2353789859@qq.com>
2023-01-24 00:23:01 +0800

    update

--- Java/test11/src/test11_5/InterpolationSearchTest.java
+++ Java/test11/src/test11_5/InterpolationSearchTest.java
@@ -3,38 +3,33 @@ package test11_5;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class BinarySearchTest {
+public class InterpolationSearchTest {
     public static void main(String[] args) {
-        int[] ints = new int[]{11, 22,22,22,22,22, 33, 44, 55, 66, 77, 88, 99};
-        System.out.println(BinarySearch.startSearchAll(ints, 10));
+        int[] ints = {1, 2,2,2, 3, 4, 5, 6, 7, 8, 9, 10};
+        System.out.println(InterpolationSearch.startSearch(ints, 100));
     }
 }
 
-class BinarySearch {
+class InterpolationSearch {
     //如果有相同元素，只找一个
     public static int startSearch(int[] arr, int searchedNum) {
         return search(arr, 0, arr.length - 1, searchedNum);
     }
-    /**
-     * @param left 表示区间起点在原数组中的下标
-     * @param right 表示区间终点在原数组的下标
-     */
     public static int search(int[] arr, int left, int right, int searchedNum) {
-        //如果区间内已经没有元素了，说明找不到，返回-1
-        //如果要找的数比数组的最小值更小，或最大值更大，那么肯定找不到，直接返回-1
+        //注意：if内的第二、第三个条件不可忽略，因为searchedNum直接参与到middle下标值的计算中
+        //如果searchedNum过大或过小，可能导致计算出的middle下标越界！
         if (left > right || searchedNum < arr[0] || searchedNum > arr[arr.length - 1]) {
             return -1;
         }
-        //计算区间中间的下标
-        int middle = (left + right) / 2;
+
+        //与二分查找的主要区别就是，计算middle下标的表达式不同
+        int middle = left + (right - left) * (searchedNum - arr[left]) / (arr[right] - arr[left]);
+
         if (arr[middle] > searchedNum) {
-            //说明要找的数在左半区间，因此向左递归
             return search(arr, left, middle - 1, searchedNum);
         } else if (arr[middle] < searchedNum) {
-            //说明要找的数在右半区间，向右递归
             return search(arr, middle + 1, right, searchedNum);
         } else {
-            //arr[middle]就是要找的数，结束递归并返回下标
             return middle;
         }
     }
@@ -47,13 +42,12 @@ class BinarySearch {
         if (left > right || searchedNum < arr[0] || searchedNum > arr[arr.length - 1]) {
             return new ArrayList<Integer>();
         }
-        int middle = (left + right) / 2;
+        int middle = left + (right - left) * (searchedNum - arr[left]) / (arr[right] - arr[left]);
         if (arr[middle] > searchedNum) {
             return searchAll(arr, left, middle - 1, searchedNum);
         } else if (arr[middle] < searchedNum) {
             return searchAll(arr, middle + 1, right, searchedNum);
         } else {
-            //找到后，还要向前后找，找到全部与其相同的元素
             ArrayList<Integer> nums = new ArrayList<>();
             nums.add(middle);
             int tempIndex = middle - 1;
