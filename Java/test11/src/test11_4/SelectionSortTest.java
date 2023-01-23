df94d6067a81bae251b64a59b05d36b315073fd4
LandmineFly <2353789859@qq.com>
2023-01-23 16:10:07 +0800

    update

--- Java/test11/src/test11_4/SelectionSortTest.java
+++ Java/test11/src/test11_4/SelectionSortTest.java
@@ -16,11 +16,16 @@ public class SelectionSortTest {
 }
 
 class SelectionSort{
+    //单次循环中找到的最小元素的下标
     public static int index;
+    //单次循环中找到的最小元素值
     public static int value;
 
+    //正序
     public static void sortInOrder(int[] arr){
         for(int i = 0; i<arr.length - 1; i++){
+            //找单次循环中的最小元素
+            //先假设这次循环中的最小元素就是arr[n]
             index = i;
             value = arr[i];
             for(int j = i + 1; j<arr.length; j++){
@@ -29,6 +34,7 @@ class SelectionSort{
                     value = arr[j];
                 }
             }
+            //如果这次循环中的最小元素不是arr[n]，则交换
             if(value != i){
                 arr[index] = arr[i];
                 arr[i] = value;
@@ -36,6 +42,7 @@ class SelectionSort{
         }
     }
 
+    //倒序
     public static void sortInReverseOrder(int[] arr){
         for(int i = 0; i<arr.length - 1; i++){
             index = i;
