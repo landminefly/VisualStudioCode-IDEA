3974c187075d303bbc27dbd09dfbcb165178d5ba
LandmineFly <2353789859@qq.com>
2023-01-31 22:22:49 +0800

    update

--- Java/test11/src/test11_8/HuffmanTreeTest.java
+++ Java/test11/src/test11_8/HuffmanTreeTest.java
@@ -8,14 +8,13 @@ public class HuffmanTreeTest {
         int[] ints = {1, 3, 6, 7, 9, 5, 1};
         HuffmanTree huffmanTree = new HuffmanTree(ints);
         huffmanTree.preorder();
-        huffmanTree.inorder();
-        huffmanTree.postorder();
     }
 }
 
 class HuffmanTree {
     public Node root;
 
+    //构造赫夫曼树
     public HuffmanTree(int[] arr) {
         ArrayList<Node> nodes = new ArrayList<>();
         for (int i : arr) {
@@ -23,13 +22,16 @@ class HuffmanTree {
         }
         while (nodes.size() > 1) {
             Collections.sort(nodes);
+            //从集合中选出两棵权最小的二叉树，合并成新的二叉树
             Node leftNode = nodes.get(0);
             Node rightNode = nodes.get(1);
             Node parent = new Node(leftNode.weight + rightNode.weight);
             parent.left = leftNode;
             parent.right = rightNode;
+            //从集合中删除两个旧的二叉树
             nodes.remove(0);
             nodes.remove(0);
+            //并加入新的二叉树
             nodes.add(parent);
         }
         root = nodes.get(0);
@@ -44,27 +46,9 @@ class HuffmanTree {
         }
         System.out.println("输出完毕");
     }
-
-    public void inorder() {
-        System.out.println("开始输出");
-        if (root == null) {
-            return;
-        } else {
-            root.inorder();
-        }
-        System.out.println("输出完毕");
-    }
-    public void postorder() {
-        System.out.println("开始输出");
-        if (root == null) {
-            return;
-        } else {
-            root.postorder();
-        }
-        System.out.println("输出完毕");
-    }
 }
 
+//需要实现Comparable接口
 class Node implements Comparable<Node> {
     public int weight;
     public Node left;
@@ -78,26 +62,6 @@ class Node implements Comparable<Node> {
         if (right != null) {
             right.preorder();
         }
-    }
-
-    public void inorder() {
-        if (left != null) {
-            left.inorder();
-        }
-        System.out.println(this);
-        if (right != null) {
-            right.inorder();
-        }
-    }
-
-    public void postorder() {
-        if (left != null) {
-            left.postorder();
-        }
-        if (right != null) {
-            right.postorder();
-        }
-        System.out.println(this);
     }
 
     public Node(int weight) {
