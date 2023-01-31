bd7b7dc822f81062c1c0b9f9abed0f789af3417d
LandmineFly <2353789859@qq.com>
2023-02-01 00:22:52 +0800

    update

--- Java/test11/src/test11_9/BinarySortTreeTest.java
+++ Java/test11/src/test11_9/BinarySortTreeTest.java
@@ -40,6 +40,7 @@ class BinarySortTree {
         System.out.println("输出完毕");
     }
 
+    //插入
     public void add(Node node) {
         if (node == null) {
             return;
@@ -51,41 +52,50 @@ class BinarySortTree {
         }
     }
 
-    public Node search(int keyValue){
+    //查找指定节点
+    public Node search(int keyValue) {
         return searchWithParent(keyValue);
     }
 
+    //父节点指针，用于记录searchWithParent查找到的父节点
     private Node parent;
+
+    /**
+     * 查找指定节点及其父节点
+     * 可以看到，当且仅当 parent == null 时，表示要找的节点就是根节点
+     * 在delete方法中会用到该逻辑
+     */
     private Node searchWithParent(int keyValue) {
         parent = null;
         if (root == null) {
             return null;
         }
-        return searchWithParent(root,keyValue);
+        return searchWithParent(root, keyValue);
     }
 
     private Node searchWithParent(Node node, int keyValue) {
-        if(node.value == keyValue){
+        if (node.value == keyValue) {
             return node;
         } else if (node.value > keyValue) {
-            if(node.left == null){
+            if (node.left == null) {
                 return null;
             }
             parent = node;
-            return searchWithParent(node.left,keyValue);
-        }else{
+            return searchWithParent(node.left, keyValue);
+        } else {
             if (node.right == null) {
                 return null;
             }
             parent = node;
-            return searchWithParent(node.right,keyValue);
+            return searchWithParent(node.right, keyValue);
         }
     }
 
-    private int deleteMinNodeOfRightSubtree(Node node){
+    //删除指定节点的右子树中的最小值节点，并返回该最小值
+    private int deleteMinNodeOfRightSubtree(Node node) {
         Node temp = node;
         Node tempParent = node;
-        while(temp.left != null){
+        while (temp.left != null) {
             tempParent = temp;
             temp = temp.left;
         }
@@ -93,43 +103,63 @@ class BinarySortTree {
         return temp.value;
     }
 
-    public boolean delete(int keyValue){
+    //删除指定节点，删除成功返回true，反之返回false
+    public boolean delete(int keyValue) {
+        //先找到该节点及其父节点
         Node nodeToDelete = searchWithParent(keyValue);
-        if(nodeToDelete == null){
+        if (nodeToDelete == null) {
             return false;
         }
-        if(nodeToDelete.left == null && nodeToDelete.right == null){
-            if(parent == null){
+        //如果待删除节点是叶子节点
+        if (nodeToDelete.left == null && nodeToDelete.right == null) {
+            //如果是根节点，直接将root置空
+            if (parent == null) {
                 root = null;
-            }else{
+            }
+            //否则将父节点指向它的指针置空
+            else {
                 if (parent.left != null && parent.left.value == keyValue) {
                     parent.left = null;
-                }else{
+                } else {
                     parent.right = null;
                 }
             }
             return true;
-        }else if(nodeToDelete.left != null && nodeToDelete.right != null){
+        }
+        //如果待删除节点是分支点，且左右节点都不为空
+        else if (nodeToDelete.left != null && nodeToDelete.right != null) {
+            //找到其右子树的最小值节点，删除该最小值节点，再将该最小值赋值给待删除节点
             nodeToDelete.value = deleteMinNodeOfRightSubtree(nodeToDelete);
             return true;
-        }else{
-            if(nodeToDelete.left != null){
+        }
+        //如果待删除节点是分支点，且只有一个子节点
+        else {
+            //如果左子节点不为空
+            if (nodeToDelete.left != null) {
+                //如果是根节点，将root置为root的左子节点
                 if (parent == null) {
                     root = root.left;
-                }else{
-                    if(parent.left.value == keyValue){
+                }
+                //否则将父节点指向它的指针指向它的左子节点
+                else {
+                    if (parent.left.value == keyValue) {
                         parent.left = nodeToDelete.left;
-                    }else{
+                    } else {
                         parent.right = nodeToDelete.left;
                     }
                 }
-            }else{
-                if(parent == null){
+            }
+            //如果右子节点不为空
+            else {
+                //如果是根节点，将root置为root的右子节点
+                if (parent == null) {
                     root = root.right;
-                }else{
+                }
+                //否则将父节点指向它的指针指向它的右子节点
+                else {
                     if (parent.left.value == keyValue) {
                         parent.left = nodeToDelete.right;
-                    }else{
+                    } else {
                         parent.right = nodeToDelete.right;
                     }
                 }
