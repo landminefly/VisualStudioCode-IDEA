ee0ad54e4f3736f4b3cd2dcf1ae08fd9194fd2b5
LandmineFly <2353789859@qq.com>
2023-03-04 21:05:14 +0800

    update

--- Java/test11/src/test11_12/GreedyTest.java
+++ Java/test11/src/test11_12/GreedyTest.java
@@ -6,35 +6,35 @@ import java.util.HashSet;
 
 public class GreedyTest {
     public static void main(String[] args) {
+        //存放所有广播台及其能覆盖的地区，广播台以String的形式存储，其覆盖的地区以HashSet<String>的形式存储
         HashMap<String, HashSet<String>> allBroadcasts = new HashMap<>();
 
         HashSet<String> hashSet1 = new HashSet<>();
         hashSet1.add("北京");
         hashSet1.add("上海");
         hashSet1.add("天津");
+        allBroadcasts.put("B1", hashSet1);
 
         HashSet<String> hashSet2 = new HashSet<>();
         hashSet2.add("广州");
         hashSet2.add("北京");
         hashSet2.add("深圳");
+        allBroadcasts.put("B2", hashSet2);
 
         HashSet<String> hashSet3 = new HashSet<>();
         hashSet3.add("成都");
         hashSet3.add("上海");
         hashSet3.add("杭州");
+        allBroadcasts.put("B3", hashSet3);
 
         HashSet<String> hashSet4 = new HashSet<>();
         hashSet4.add("上海");
         hashSet4.add("天津");
+        allBroadcasts.put("B4", hashSet4);
 
         HashSet<String> hashSet5 = new HashSet<>();
         hashSet5.add("杭州");
         hashSet5.add("大连");
-
-        allBroadcasts.put("B1", hashSet1);
-        allBroadcasts.put("B2", hashSet2);
-        allBroadcasts.put("B3", hashSet3);
-        allBroadcasts.put("B4", hashSet4);
         allBroadcasts.put("B5", hashSet5);
 
         //存放还没被覆盖的地区，初始化包含全部地区
@@ -48,12 +48,14 @@ public class GreedyTest {
         areasRemained.add("杭州");
         areasRemained.add("大连");
 
-        //存放已经选择的广播
+        //存放已经选择的广播，初始化为空集合
         ArrayList<String> broadcastSelected = new ArrayList<>();
-        //临时集合，在遍历剩余广播时，存放该广播覆盖地区与还没被覆盖的地区的交集
+        //临时集合，在遍历剩余广播时，存放当前遍历广播的覆盖地区与还没被覆盖地区的交集
         ArrayList<String> temp = new ArrayList<>();
 
+        //在一次遍历中，存放能覆盖最多未覆盖地区的广播台
         String maxBroadcast = null;
+        //在一次遍历中，存放上述广播台所覆盖的地区数
         int maxAreaCount = 0;
         while (areasRemained.size() != 0) {
             maxBroadcast = null;
@@ -71,6 +73,7 @@ public class GreedyTest {
             if (maxBroadcast == null) {
                 throw new RuntimeException("emm,贪心算法好像出错了");
             }
+            //将maxBroadcast加入broadcastSelected中，并将maxBroadcast新覆盖的地区从areasRemained中移除
             broadcastSelected.add(maxBroadcast);
             areasRemained.removeAll(allBroadcasts.get(maxBroadcast));
         }
