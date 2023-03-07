c0d7c80cc9638f08c6eca7aaa226ffcb4c74ed62
LandmineFly <2353789859@qq.com>
2023-03-08 00:26:51 +0800

    update

---
+++ Java/test11/src/test11_12/DijkstraTest.java
@@ -0,0 +1,122 @@
+package test11_12;
+
+import java.util.Arrays;
+
+public class DijkstraTest {
+    public static final int INF = 65535;
+
+    public static void main(String[] args) {
+        char[] vertexes = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        int[][] edges = new int[][]{{INF, 12, INF, INF, INF, 16, 14},
+                                    {12, INF, 10, INF, INF, 7, INF},
+                                    {INF, 10, INF, 3, 5, 6, INF},
+                                    {INF, INF, 3, INF, 4, INF, INF},
+                                    {INF, INF, 5, 4, INF, 2, 8},
+                                    {16, 7, 6, INF, 2, INF, 9},
+                                    {14, INF, INF, INF, 8, 9, INF}};
+        Graph3 graph = new Graph3(vertexes, edges);
+        Dijkstra dijkstra = new Dijkstra(graph);
+        dijkstra.nearest(0);
+        System.out.println(Arrays.toString(dijkstra.distance));
+        for (int i = 0; i < graph.VCount; i++) {
+            int index = i;
+            while (index != 0) {
+                System.out.print(graph.translateIndexToVertex(index) + " <- ");
+                index = dijkstra.preNode[index];
+            }
+            System.out.println(graph.translateIndexToVertex(index));
+        }
+    }
+}
+
+class Dijkstra {
+    public Graph3 graph;
+    public boolean[] visited;
+    public int[] preNode;
+    public int[] distance;
+
+    public static final int INF = 65535;
+
+    public Dijkstra(Graph3 graph) {
+        this.graph = graph;
+        visited = new boolean[graph.VCount];
+        preNode = new int[graph.VCount];
+        Arrays.fill(preNode, -1);
+        distance = new int[graph.VCount];
+        Arrays.fill(distance, INF);
+    }
+
+    public void nearest(int index) {
+        visited[index] = true;
+        distance[index] = 0;
+        update(index);
+        for (int i = 0; i < graph.VCount - 1; i++) {
+            update(visit());
+        }
+    }
+
+    public void update(int index) {
+        int tempDis;
+        for (int i = 0; i < graph.VCount; i++) {
+            tempDis = distance[index] + graph.edgesWithWeight[index][i];
+            if (!visited[i] && tempDis < distance[i]) {
+                distance[i] = tempDis;
+                preNode[i] = index;
+            }
+        }
+    }
+
+    public int visit() {
+        int minDis = INF;
+        int minNode = -1;
+        for (int i = 0; i < graph.VCount; i++) {
+            if (!visited[i] && distance[i] < minDis) {
+                minDis = distance[i];
+                minNode = i;
+            }
+        }
+        visited[minNode] = true;
+        return minNode;
+    }
+}
+
+class Graph3 {
+    int VCount;
+    int ECount;
+    char[] vertexes;
+    int[][] edgesWithWeight;
+    public static final int INF = 65535;
+
+    public Graph3(char[] vertexes, int[][] edgesWithWeight) {
+        this.vertexes = vertexes;
+        this.edgesWithWeight = edgesWithWeight;
+        this.VCount = vertexes.length;
+        for (int i = 0; i < VCount; i++) {
+            for (int j = i + 1; j < VCount; j++) {
+                if (edgesWithWeight[i][j] != INF) {
+                    ECount++;
+                }
+            }
+        }
+    }
+
+    //将顶点值转换为顶点下标
+    public int translateVertexToIndex(char c) {
+        for (int i = 0; i < vertexes.length; i++) {
+            if (c == vertexes[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //将顶点下标转换为顶点值
+    public char translateIndexToVertex(int index) {
+        if (index < VCount && index >= 0) {
+            return vertexes[index];
+        } else {
+            throw new RuntimeException("下标错误，转换失败");
+        }
+    }
+}
+
---
+++ Java/test11/src/test11_12/FloydTest.java
@@ -0,0 +1,154 @@
+package test11_12;
+
+import java.util.Arrays;
+
+public class FloydTest {
+    public static final int INF = 65535;
+
+    public static void main(String[] args) {
+        char[] vertexes = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        int[][] edges = new int[][]{{0, 12, INF, INF, INF, 16, 14},
+                                    {12, 0, 10, INF, INF, 7, INF},
+                                    {INF, 10, 0, 3, 5, 6, INF},
+                                    {INF, INF, 3, 0, 4, INF, INF},
+                                    {INF, INF, 5, 4, 0, 2, 8},
+                                    {16, 7, 6, INF, 2, 0, 9},
+                                    {14, INF, INF, INF, 8, 9, 0}};
+        Graph4 graph = new Graph4(vertexes, edges);
+
+        Floyd floyd = new Floyd(graph);
+        // for (int[] i : floyd.preNode) {
+        //     for (int j : i) {
+        //         System.out.print(j + "\t");
+        //     }
+        //     System.out.println();
+        // }
+        //
+        // System.out.println();
+        //
+        // for (int[] i : floyd.distance) {
+        //     for (int j : i) {
+        //         if (j == INF) {
+        //             System.out.print("INF\t");
+        //         } else {
+        //             System.out.print(j + "\t");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+        //
+        // System.out.println();
+
+        floyd.nearest();
+        // for (int[] i : floyd.preNode) {
+        //     for (int j : i) {
+        //         System.out.print(j + "\t");
+        //     }
+        //     System.out.println();
+        // }
+        //
+        // System.out.println();
+        //
+        // for (int[] i : floyd.distance) {
+        //     for (int j : i) {
+        //         if (j == INF) {
+        //             System.out.print("INF\t");
+        //         } else {
+        //             System.out.print(j + "\t");
+        //         }
+        //     }
+        //     System.out.println();
+        // }
+
+        int i = 0;
+        int j = 3;
+        while(j != i){
+            System.out.print(graph.translateIndexToVertex(j) + " <- ");
+            j = floyd.preNode[i][j];
+        }
+        System.out.print(graph.translateIndexToVertex(i));
+    }
+}
+
+class Floyd {
+    public Graph4 graph;
+    public int[][] distance;
+    public int[][] preNode;
+
+    public static final int INF = 65535;
+
+    public Floyd(Graph4 graph) {
+        this.graph = graph;
+        preNode = new int[graph.VCount][graph.VCount];
+        for (int[] i : preNode) {
+            Arrays.fill(i, -1);
+        }
+        distance = new int[graph.VCount][];
+        for (int i = 0; i<graph.VCount; i++) {
+            distance[i] = Arrays.copyOf(graph.edgesWithWeight[i], graph.VCount);
+        }
+        for (int i = 0; i < graph.VCount; i++) {
+            for (int j = i + 1; j < graph.VCount; j++) {
+                if (graph.edgesWithWeight[i][j] != INF) {
+                    preNode[i][j] = i;
+                    preNode[j][i] = j;
+                }
+            }
+        }
+    }
+
+    public void nearest(){
+        int tempDis;
+        for(int k = 0; k< graph.VCount;k++){
+            for(int i = 0; i< graph.VCount;i++){
+                for(int j = 0; j< graph.VCount;j++){
+                    tempDis = distance[i][k] + distance[k][j];
+                    if(tempDis < distance[i][j]){
+                        distance[i][j] = tempDis;
+                        preNode[i][j] = preNode[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
+
+class Graph4 {
+    int VCount;
+    int ECount;
+    char[] vertexes;
+    int[][] edgesWithWeight;
+    public static final int INF = 65535;
+
+    public Graph4(char[] vertexes, int[][] edgesWithWeight) {
+        this.vertexes = vertexes;
+        this.edgesWithWeight = edgesWithWeight;
+        this.VCount = vertexes.length;
+        for (int i = 0; i < VCount; i++) {
+            for (int j = i + 1; j < VCount; j++) {
+                if (edgesWithWeight[i][j] != INF) {
+                    ECount++;
+                }
+            }
+        }
+    }
+
+    //将顶点值转换为顶点下标
+    public int translateVertexToIndex(char c) {
+        for (int i = 0; i < vertexes.length; i++) {
+            if (c == vertexes[i]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //将顶点下标转换为顶点值
+    public char translateIndexToVertex(int index) {
+        if (index < VCount && index >= 0) {
+            return vertexes[index];
+        } else {
+            throw new RuntimeException("下标错误，转换失败");
+        }
+    }
+}
